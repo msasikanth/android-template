1a2ff8ff69b9a5ad36e88a85469b69a05e9df069
Sasikanth Miriyampalli <hello@sasikanth.dev>
2021-10-17 11:09:35 +0530

    Use version catalog for Gradle dependencies management
    https://sasikanth.dev/posts/gradle-version-catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,7 @@ android {
     compose = true
   }
   composeOptions {
-    val composeVersion: String by rootProject.extra
-
-    kotlinCompilerExtensionVersion = composeVersion
+    kotlinCompilerExtensionVersion = libs.versions.compose.get()
   }
   packagingOptions {
     resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
@@ -46,19 +44,17 @@ android {
 }
 
 dependencies {
-  val composeVersion: String by rootProject.extra
+  debugImplementation(libs.compose.ui.tooling)
 
-  implementation("androidx.core:core-ktx:1.6.0")
-  implementation("androidx.appcompat:appcompat:1.3.1")
-  implementation("com.google.android.material:material:1.4.0")
-  implementation("androidx.compose.ui:ui:$composeVersion")
-  implementation("androidx.compose.material:material:$composeVersion")
-  implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-  implementation("androidx.activity:activity-compose:1.3.1")
-  testImplementation("junit:junit:4.+")
-  androidTestImplementation("androidx.test.ext:junit:1.1.3")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-  androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-  debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+  implementation(libs.appcompat)
+  implementation(libs.bundles.compose)
+  implementation(libs.core)
+  implementation(libs.lifecycle.runtime)
+  implementation(libs.material)
+
+  testImplementation(libs.junit)
+
+  androidTestImplementation(libs.androidx.junit)
+  androidTestImplementation(libs.compose.ui.test)
+  androidTestImplementation(libs.espresso)
 }
